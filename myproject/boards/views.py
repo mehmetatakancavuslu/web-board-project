b317a7c7b8fb0a2f2ea664b217ed928740fb923b
atakancavuslu <mcavuslu15@ku.edu.tr>
2020-04-07 22:59:59 +0300

    Implement models, and home.html template.

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Board
 
 # Create your views here.
+def home(request):
+    boards = Board.objects.all()
+    context = {'boards': boards}
+    return render(request, 'boards/home.html', context)
