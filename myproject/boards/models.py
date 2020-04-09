5896489c9be6e21483d819a0fc49de3e9fd11dfc
atakancavuslu <mcavuslu15@ku.edu.tr>
2020-04-09 21:10:22 +0300

    Add more tests and add docstrings for views and models.

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Board(models.Model):
+    """
+    Stores boards created by admin users.
+    """
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
 
@@ -10,6 +13,9 @@ class Board(models.Model):
         return self.name
 
 class Topic(models.Model):
+    """
+    Stores a single topic, under a specific Board  model, created by a user.
+    """
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, on_delete=models.CASCADE,
@@ -18,6 +24,10 @@ class Topic(models.Model):
                                 related_name='topics')
 
 class Post(models.Model):
+    """
+    Stores a single post entry which lives in a topic. Can be created and
+    edited by a user.
+    """
     message = models.TextField(max_length=4000)
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE,
                               related_name='posts')
--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -4,10 +4,17 @@ from .models import Board
 
 # Create your views here.
 def home(request):
+    """
+    Home view showing all the boards and board related informations.
+    """
     boards = Board.objects.all()
     context = {'boards': boards}
     return render(request, 'boards/home.html', context)
 
 def board_topics(request, pk):
+    """
+    Topics view showing the topics created in the given specific board and
+    the relevant information of these topics.
+    """
     board = Board.objects.get(pk=pk)
     return render(request, 'boards/topics.html', {'board': board})
